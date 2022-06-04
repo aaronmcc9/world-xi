88d8b7d52233df421040537977454c7da6aa7ff1
Aaron McCarthy <Aaron.McCarthy@logibec.com>
2022-06-04 16:38:23 -0700

    setting up initial components and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
+import { AddPlayerComponent } from './players/add-player/add-player.component';
+import { PlayerEditComponent } from './players/player-edit/player-edit.component';
+import { PlayersDetailComponent } from './players/players-detail/players-detail.component';
+import { PlayersComponent } from './players/players.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/players', pathMatch:"full"},
+  {path:'players', component:PlayersComponent, children:[
+    {path:'new', component: AddPlayerComponent},
+    {path:':id', component: PlayersDetailComponent},
+    {path:':id/edit', component: PlayerEditComponent},
+  ]},
+  {path:'team', component:TeamComponent},
+  {path:'auth', component:AuthComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { AuthComponent } from './auth/auth/auth.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayersListComponent } from './players/players-list/players-list.component';
+import { PlayersDetailComponent } from './players/players-detail/players-detail.component';
+import { AddPlayerComponent } from './players/add-player/add-player.component';
+import { PlayerEditComponent } from './players/player-edit/player-edit.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    PlayersComponent,
+    PlayersListComponent,
+    PlayersDetailComponent,
+    AddPlayerComponent,
+    PlayerEditComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,

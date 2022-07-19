028b3a4536626b624902312e97648cfe113a4c18
Aaron McCarthy <Aaron.McCarthy@logibec.com>
2022-07-19 12:00:23 -0700

    minor fix to team player positions componenet

--- src/app/team/team-list/players-in-position/players-in-position.component.ts
+++ src/app/team/team-list/players-in-position/players-in-position.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Position } from 'src/app/players/player-position';
 import { Player } from 'src/app/players/player.model';
 import { PlayersService } from 'src/app/players/players.service';
+import { faUpRightFromSquare} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-players-in-position',
@@ -11,6 +12,14 @@ import { PlayersService } from 'src/app/players/players.service';
 export class PlayersInPositionComponent implements OnInit {
   @Input('position') position: number = 0;
   players: Player[] = [];
+  error = '';
+  loading = false;
+
+  //icons
+  selectIcon = faUpRightFromSquare
+
+
+
 
   constructor(private playersService: PlayersService) { }
 
@@ -18,10 +27,34 @@ export class PlayersInPositionComponent implements OnInit {
 
     this.players = this.playersService.players
       .filter(p => {
-        return p.position = Position[this.position];
+        return p.position == Position[this.position];
       });
 
-      console.log(this.players);
+
+    if (this.players === null || this.players.length === 0)
+      this.fetchPlayers();
+  }
+
+  fetchPlayers() {
+    this.loading = true;
+
+    this.playersService.fetchAllPlayers()
+      .subscribe({
+        next: players => {
+
+          this.players = players.filter(p => {
+            return p.position == Position[this.position];
+          });
+
+          this.loading = false;
+
+        },
+        error: message => {
+          this.error = message;
+          this.loading = false;
+
+        }
+      });
   }
 
 }

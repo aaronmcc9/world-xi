1297cd82fe38e0e47c95cebef93109ccdf86fe3e
aaronmcc9 <aaronmcc9600@gmail.com>
2025-10-29 22:37:17 -0700

    minor changes

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -2,96 +2,92 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faFutbolBall } from '@fortawesome/free-solid-svg-icons';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AlertType } from 'src/app/alert/alert-type.enum';
 import { AlertService } from 'src/app/alert/alert.service';
 import { AuthApiService } from 'src/app/api/auth/auth-api.service';
 import { ServiceResponse } from 'src/app/api/Common/service-response.dto';
 import { AuthService } from './auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { TranslateService } from "@ngx-translate/core";
+
 
 export interface UserRequestDto {
-  email: string,
-  password: string
+    email: string,
+    password: string
 }
 
 interface FormValue {
-  email: FormControl<string>,
-  password: FormControl<string>
+    email: FormControl<string>,
+    password: FormControl<string>
 }
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  error = '';
-  isLogin = true;
-  form!: FormGroup<FormValue>;
+    error = '';
+    isLogin = true;
+    form!: FormGroup<FormValue>;
 
-  //icons
-  ball = faFutbolBall;
+    //icons
+    ball = faFutbolBall;
 
-  constructor(private authService: AuthService, private router: Router,
-    private authApiService: AuthApiService, private alertService: AlertService) { }
+    constructor(private authService: AuthService, private router: Router,
+        private authApiService: AuthApiService, private alertService: AlertService,
+        private translateService: TranslateService) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.form = new FormGroup<FormValue>({
-      email: new FormControl<string>('', {
-        nonNullable: true,
-        validators: [Validators.email, Validators.required, Validators.minLength(8)]
-      },),
-      password: new FormControl<string>('', {
-        nonNullable: true,
-        validators: [Validators.required, Validators.minLength(5)]
-      })
-    });
-  }
-
-  onSubmit() {
-
-    if (!this.form.valid) {
-      this.form.markAsDirty();
-      return;
+        this.form = new FormGroup<FormValue>({
+            email: new FormControl<string>('', {
+                nonNullable: true,
+                validators: [Validators.email, Validators.required, Validators.minLength(8)]
+            },),
+            password: new FormControl<string>('', {
+                nonNullable: true,
+                validators: [Validators.required, Validators.minLength(5)]
+            })
+        });
     }
 
-    let authObservable: Observable<ServiceResponse<string>>;
-    let user = this.getDto();
+    onSubmit() {
 
+        if (!this.form.valid) {
+            this.form.markAsDirty();
+            return;
+        }
 
-    if (this.isLogin) {
-      authObservable = this.authApiService.login(user);
+        let authObservable: Observable<ServiceResponse<string>>;
+        let user = this.getDto();
+
+        authObservable = this.isLogin ? this.authApiService.login(user) : this.authApiService.createAccount(user);
+        authObservable.subscribe({
+            next: (res: ServiceResponse<string>) => {
+                this.authService.setToken(res.data)
+                this.router.navigate(['players']);
+                this.form.reset();
+            },
+            error: (res: HttpErrorResponse) => {
+                this.alertService.toggleAlert(res.error ?? this.translateService.instant("AN_UNKNOWN_ERROR_OCCURRED"), AlertType.Danger, res.error.message)
+            }
+        })
+
     }
-    else {
-      authObservable = this.authApiService.createAccount(user)
+
+    onSwitchMode() {
+        this.isLogin = !this.isLogin;
     }
 
-    authObservable.subscribe({
-      next: (res: ServiceResponse<string>) => {
-        this.authService.setToken(res.data)
-        this.router.navigate(['players']);
-      },
-      error: (res: HttpErrorResponse) => {
-          this.alertService.toggleAlert('', AlertType.Danger, res.error.message)
-      }
-    })
+    private getDto(): UserRequestDto {
+        let formValue = this.form.getRawValue();
 
-    this.form.reset();
-  }
-
-  onSwitchMode() {
-    this.isLogin = !this.isLogin;
-  }
-
-  private getDto(): UserRequestDto {
-    let formValue = this.form.getRawValue();
-
-    return {
-      email: formValue.email,
-      password: formValue.password
-    } as UserRequestDto;
-  }
+        return {
+            email: formValue.email,
+            password: formValue.password
+        } as UserRequestDto;
+    }
 
 }
